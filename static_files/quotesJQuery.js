3393bf00dfd17f828d98785bf2ec7822db4a5359
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-09-25 21:17:49 -0500

    Update quotesJQuery.js
    I have made some coding adjustments today (September 25, 2019), I am most familiar with Javascript in HTML Dom then JQuery shorthand.

--- static_files/quotesJQuery.js
+++ static_files/quotesJQuery.js
@@ -1,12 +1,10 @@
-$(doucment).ready(function(){
+$(document).ready(function() {
     console.log('Testing Server Browser in order to script JQuery AJAX.');
 
 
     $('#quoterProfileButton').click(function() {
         const requestURL = 'users/' + $('lastNameTextbox').val();
-        console.log('Ms. Ajai you making an AJAX request to: ', requestURL);
-
-
+        console.log('Ms. Ajai, you making an AJAX request to: ', requestURL);
             
 
         $.ajax ({
@@ -16,17 +14,35 @@ $(doucment).ready(function(){
             success: function(data) {
                 console.log('You have received successful!', data);
 
-                // Apply Error Case, if analysis statement.
+                // Apply Error Case, if statement is analysis.
                 if (data.LastName && data.AuthorPicture) {
                     $('#status').html('Ms. Ajai, you have successfully retrieve data at URL: ' + requestURL);
-                    $('#lastNameTextbox').html("Quoter's Last Name: " + data.LastName);                    
+                    $('#QuoteDiv').html("Quoter's Last Name: " + data.Quote);
+                    $('#QuoterImage').attr('src', data.AuthorPicture).arttr('width', '300px');                    
                 }
                 else {
                     $('#status').html('ERROR: Ms. Ajai, your requested data could not be retrieved at URL: ' + requestURL);
+                    $('#QuoteDiv').html(' ');
+                    $('#QuoterImage').attr('src', ' ').attr('width', '0px');    
+
                 }
 
             }    
 
         });     
     });
+
+        $('#allQuoterButton').click(function() {
+            $.ajax ({
+                url: 'users/',
+                type: 'GET',
+                dataType: 'json',
+                success: function(data) {
+                    console.log('You have received data!', data);
+                    $('#status').html('All users: ' + data);
+                }
+        });
+
+    });
+
 });
