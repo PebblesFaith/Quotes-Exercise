23a77370e4abd0d87cb3effe43cd88da46d20924
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-09-26 20:20:29 -0500

    Update serverJQuery.js
    Hi Developers:
    
    I am almost completed my Module 3, Building Functional Prototypes Using Node.js  for Trying to code a SQL database assignment from backend RESTful API that communicates to my web application front-end. However, I am having problems with computer hackers constantly hacking onto my desktop computer and unlawfully changing my programmatic languages  in order to cause mm to have incorrect coding errors.  I had noticed, this a few times today, September 26, 2019.

--- serverJQuery.js
+++ serverJQuery.js
@@ -1,22 +1,25 @@
-const express = require('express');
-const quotesApp = express();
 
-quotesApp.use(express.static('static_files'));
+
+// Import the Express Library.
+const express = require('express');//
+const quotesApp = express();
 
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('Quotes.db');
 
+quotesApp.use(express.static('static_files'));
+
 const port = 3000;
+
 quotesApp.listen(port, function() {
     console.log('Server to Create Table is listening onto URL at http://localhost: ' + port + '.');
-})
-
+});
 
 
     
-quotesApp.get('/users/:quoteId', function(request, response) {
+quotesApp.get('/users/:lastnameId', function(request, response) {
     // Matches ':userId' above.
-    const lastnameToLookUp = request.params.quoteId;
+    const lastnameToLookUp = request.params.lastnameId;
     // SQL Query
     db.all(
         'SELECT * FROM Quotism WHERE LastName = $LastName',
@@ -25,7 +28,7 @@ quotesApp.get('/users/:quoteId', function(request, response) {
         $LastName: lastnameToLookUp
         },      
   
-        // Callback function to run when the Query  finishes.
+        // Callback function to run when the Query finishes.
         function (err, rows) {
             console.log(rows);
             if (rows.length > 0) {
@@ -46,12 +49,13 @@ quotesApp.get('/users', function(request, response) {
     db.all('SELECT LastName FROM Quotism', function (err, rows) {
 
         console.log(rows);
-        const allQuoter = rows.map(e => e.LastName);
-        console.log(allQuoter);
-        response.send(allQuoter); 
+        const allUserNames = rows.map(e => e.LastName);
+        console.log(allUserNames);
+        response.send(allUserNames); 
     
     });    
 });
+
 
 // POST request is for posting new data to the server.
 const bodyParser = require('body-parser');
@@ -62,10 +66,13 @@ quotesApp.post('/users', function(request, response) {
     //response.send({});
 
     db.run(
-        'INSERT INTO Quotism VALUES ($LastName, $AuthorPicture)',
+        'INSERT INTO Quotism VALUES ($Quote, $FirstName, $LastName, $YearPublished, $AuthorPicture)',
         {
-            $LastName: request.body.LastName,         
-            $AuthorPicture: request.body.AuthorPicture
+            $Quote: request.body.Quote,
+            $FirstName: request.body.FirstName,
+            $LastName: request.body.LastName,
+            $YearPublished: request.body.YearPublished,            
+            $AuthorPicture: request.body.AuthorPicture    
         },
         (err)  => {
             if (err) {
@@ -80,4 +87,9 @@ quotesApp.post('/users', function(request, response) {
     );
 
 });
+
+
+
+
+
 
