05bc288c1238c55390f5e1f79c4a4a46000b6fdd
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-09-26 20:21:03 -0500

    Update quotesJQuery.js
    Hi Developers:
    
    I am almost completed my Module 3, Building Functional Prototypes Using Node.js  for Trying to code a SQL database assignment from backend RESTful API that communicates to my web application front-end. However, I am having problems with computer hackers constantly hacking onto my desktop computer and unlawfully changing my programmatic languages  in order to cause mm to have incorrect coding errors.  I had noticed, this a few times today, September 26, 2019.

--- static_files/quotesJQuery.js
+++ static_files/quotesJQuery.js
@@ -1,48 +1,94 @@
 $(document).ready(function() {
     console.log('Testing Server Browser in order to script JQuery AJAX.');
 
-
     $('#quoterProfileButton').click(function() {
-        const requestURL = 'users/' + $('lastNameTextbox').val();
-        console.log('Ms. Ajai, you making an AJAX request to: ', requestURL);
-            
+        const requestURL = 'users/' + $('#lastNameTextbox').val();
+        console.log('You are making an AJAX request to: ', requestURL);
 
-        $.ajax ({
+        $.ajax({
             url: requestURL,
             type: 'GET',
             dataType: 'json',
             success: function(data) {
-                console.log('You have received successful!', data);
+                console.log('You have received some data!', data);
 
-                // Apply Error Case, if statement is analysis.
-                if (data.LastName && data.AuthorPicture) {
-                    $('#status').html('Ms. Ajai, you have successfully retrieve data at URL: ' + requestURL);
-                    $('#QuoteDiv').html("Quoter's Last Name: " + data.Quote);
-                    $('#QuoterImage').attr('src', data.AuthorPicture).arttr('width', '300px');                    
+                // Apply Error Case if statement analysis.
+                if (data.Quote && data.AuthorPicture) {
+                    console.log('You have successfully retrieve data at URL: ' + requestURL);
+                    $('#LastNameDiv').html("Quoter's Last Name: " + data.LastName);
+                    $('#QuoteDiv').html("Quote: " + data.Quote);
+                    $('#QuoterImage').attr('src', data.AuthorPicture).attr('width', '300px');
                 }
                 else {
-                    $('#status').html('ERROR: Ms. Ajai, your requested data could not be retrieved at URL: ' + requestURL);
-                    $('#QuoteDiv').html(' ');
-                    $('#QuoterImage').attr('src', ' ').attr('width', '0px');    
-
+                    $('#status').html('ERROR: You have requested data could not be retrieved at URL: ' + requestURL);
+                    $('#LastNameDiv').html('');
+                    $('#QuoteDiv').html('');
+                    $('#QuoterImage').attr('src', '').attr('width', '0px');
                 }
-
-            }    
-
-        });     
-    });
-
-        $('#allQuoterButton').click(function() {
-            $.ajax ({
-                url: 'users/',
-                type: 'GET',
-                dataType: 'json',
-                success: function(data) {
-                    console.log('You have received data!', data);
-                    $('#status').html('All users: ' + data);
-                }
+            }
         });
 
     });
 
+    $('#allquotersProfileButton').click(function() {
+        $.ajax({
+            url: 'users/',
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                console.log('You have received some data!', data);
+                $('#status1').html('List of all Last Name Quoters: ' + data);
+            }
+        });
+    });
+
+
+    $('#quoterInsertProfileButton').click(function() {
+        console.log('Selected insert button had been clicked!');
+        $.ajax({
+            url: 'users',
+            type: 'POST',
+            data: {
+                Quote: $('#quoteTextarea').val(),
+                FirstName: $('#firstNameTextbox').val(),
+                LastName: $('#lastNameTextbox').val(),
+                YearPublished: $('#yearPublishedTextbox').val(),
+                AuthorPicture: $('#authorPictureTextbox').val(),
+            },
+            success: function(data) {
+                $('#status2').html(data.message);
+            }
+        });
+    });
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
+
+
+
+
